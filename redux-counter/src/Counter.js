b4cdd773c7c69852d55c4d651a639972c781b59c
Maysa Orazova <[maysaorash@gmail.com]>
2021-01-31 17:10:47 -0500

    create some functions

---
+++ redux-counter/src/Counter.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+
+function Counter(){
+return <div></div>
+}
+
+const mapStateToProps = (state) => {
+
+}
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
