7caadcf33b2ae3b9f14ed53acb7e2ebc37eb8cd9
maysaorash <72366798+maysaorash@users.noreply.github.com>
2021-02-01 18:07:38 -0500

    Add files via upload

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,15 +1,30 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { increaseCount, decreaseCount } from './actions/counterAction';
 
-function Counter(){
-return <div></div>
+function Counter(props) {
+  return (
+  <div>
+      {props.count}
+      <button onClick={props.decrease}>Decrease 1</button>
+      <button onClick={props.decrease}>Decrease 5</button>
+      <button onClick={props.decrease}>Decrease 10</button>
+      <button onClick={props.increase}>Increase 1</button>
+      <button onClick={props.increase}>Increase 5</button>
+      <button onClick={props.increase}>Increase 10</button>
+
+      </div>
+      )
 }
 
 const mapStateToProps = (state) => {
-
+    return {
+        count: state.count
+    }
 }
 
 const mapDispatchToProps = {
-    
+increase: increaseCount,
+decrease: decreaseCount
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
